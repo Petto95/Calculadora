5d2dc63c9be7171c617d9c062f88c55e603f9091
Renato <renato.pijanowski@gmail.com>
2016-09-06 13:14:56 -0300

    Calculator

--- src/br/com/View/CalculadoraFrame.java
+++ src/br/com/View/CalculadoraFrame.java
@@ -1,22 +1,16 @@
 package br.com.View;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
-
 import java.awt.Color;
-
 import javax.swing.JTextField;
-
 import java.awt.Font;
-
 import javax.swing.JButton;
-
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
